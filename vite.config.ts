92586fed0c14bed855b6cf384c0f9297c0010f08
lucas oliveira <lucas.meialua@outlook.com>
2023-11-11 17:25:34 -0300

    aciton

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import env from "dotenv";
 env.config();
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/PortfolioV3",
   server: {
     open: true
   },
